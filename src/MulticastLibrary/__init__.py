b6fb10b3f451dec1045e2b88405171ac4887aaf5
ganesanluna <ganesanluna@yahoo.in>
2025-02-14 11:01:17 +0530

    Create a __init__.py

---
+++ src/MulticastLibrary/__init__.py
@@ -0,0 +1,8 @@
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+from .library import MulticastLibrary
+from .version import VERSION
+
+__version__ = VERSION
